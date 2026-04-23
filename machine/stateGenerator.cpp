f98a29eef80172b5bf10bd5dc0932679b3c3229f
Šimon Rataj <ratajs@ratajs.cz>
2026-04-23 15:00:43 +0200

    Document StateGenerator

--- machine/stateGenerator.cpp
+++ machine/stateGenerator.cpp
@@ -1,6 +1,10 @@
 #include "./stateGenerator.hpp"
 #include "../IO/unexpectedError.hpp"
 
+/*!
+ * \return The state preceding the latest state in order of generation.
+ * \throw If such a state does not exist, i.e. the current state is the first one.
+ */
 std::wstring StateGenerator::getLastState() const {
 	if(this->lastState.empty())
 		throw UnexpectedError(L"No last state yet.");
@@ -8,10 +12,16 @@ std::wstring StateGenerator::getLastState() const {
 	return this->lastState;
 };
 
+/*!
+ * \return The latest state.
+ */
 std::wstring StateGenerator::getCurrentState() const {
 	return this->currentState;
 };
 
+/*!
+ * Generate the next state name.
+ */
 void StateGenerator::increment() {
 	std::wstring::reverse_iterator it;
 
--- machine/stateGenerator.hpp
+++ machine/stateGenerator.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #include "../common.hpp"
 
+/*!
+ * This class generates state names for MachineFactory.
+ * The first state is A, next states follow alphabetically.
+ * After Z, the next states are AA, AB, AC and so on, after AZ goes BA, after ZZ goes AAA.
+ */
 class StateGenerator {
 	private:
 		std::wstring lastState;
