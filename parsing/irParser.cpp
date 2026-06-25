fe24e2d881f0c88d7de6401745f23a358ddd586c
Šimon Rataj <ratajs@ratajs.cz>
2026-06-25 14:41:53 +0200

    Clear trailing whitespaces in IR

--- parsing/irParser.cpp
+++ parsing/irParser.cpp
@@ -9,16 +9,21 @@ void IrParser::parseLine(std::wstring_view line) {
 IrParser::IrParser(const std::wstring &text, const std::back_insert_iterator<std::vector<std::unique_ptr<Warning>>> &warningIt): it(text.begin()), endIt(text.end()), warningIt(warningIt) {};
 
 InstructionCollection IrParser::parse() {
-	size_t commentSymbolPos;
+	size_t pos;
 	std::wstring_view line;
 	std::wstring::const_iterator lineStartIt;
 
 	for(lineStartIt = this->it; lineStartIt!=this->endIt; lineStartIt = std::next(this->it)) {
 		for(this->it = lineStartIt; this->it!=endIt && (*this->it)!='\n'; this->it++);
 		line = std::wstring_view(lineStartIt, this->it);
-		commentSymbolPos = line.find('#');
-		if(commentSymbolPos!=line.npos)
-			line.remove_suffix(line.size() - commentSymbolPos);
+		pos = line.find('#');
+		if(pos!=line.npos)
+			line.remove_suffix(line.size() - pos);
+		pos = line.find_last_not_of(L" \t");
+		if(pos==line.npos)
+			line.remove_suffix(line.size());
+		else
+			line.remove_suffix(line.size() - pos - 1);
 
 		this->parseLine(line);
 		this->lineNumber++;
