8aaee4f8e698d3e9122703c4dcb008cbcd706d89
Šimon Rataj <ratajs@ratajs.cz>
2026-06-30 19:47:16 +0200

    Comment ReversePseudoinstruction

--- instructions/reversePseudoinstruction.hpp
+++ instructions/reversePseudoinstruction.hpp
@@ -6,6 +6,11 @@
 #include <functional>
 #include "./instruction.hpp"
 
+/*!
+ * This is a special instruction for mirroring of all computation.
+ * The content of all tapes will be interpreted as mirrored until the next occurence of this instruction.
+ * The placement in the list of instruction is important (other instructions being between REVERSE pseudoinstructions), as well as the execution of REVERSE itself.
+ */
 class ReversePseudoinstruction: public Instruction {
 	public:
 		std::vector<size_t> listUsedTapes() const override;
